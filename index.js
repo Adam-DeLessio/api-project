eb028c59e76b9035e8fe138dc345977787acae86
Adam DeLessio <adamdelessio@gmail.com>
2019-12-29 16:44:34 -0500

    populated json file

--- index.js
+++ index.js
@@ -2,10 +2,6 @@ const express = require('express')
 const app = express()
 const breweryRoutes = require('./routes/breweryRoutes')
 
-// app.get('/', (req, res) => {
-// 	res.send('hello world')
-// })
-
 app.use('/', breweryRoutes)
 
 app.listen(3000, () => console.log('index working'))
